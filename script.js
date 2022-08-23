a7d50f5f1d905f3c09e1017fb8eeefa40c59ede6
Lakshya17verma <lakshmanverma17@gmail.com>
2022-08-23 11:44:46 +0530

    new Project
    upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+function changeSlide(direction) {
+    var currentImg = $('.active');
+    var nextImg = currentImg.next();
+    var previousImg = currentImg.prev();
+
+    if (direction == 'next') {
+        if (nextImg.length)
+            nextImg.addClass('active');
+        else
+            $('.slider img').first().addClass('active');
+    } else {
+        if (previousImg.length)
+            previousImg.addClass('active');
+        else
+            $('.slider img').last().addClass('active');
+    }
+
+    currentImg.removeClass('active');
+}
